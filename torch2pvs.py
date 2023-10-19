5d22317f9ff3180fc2ec1226559737605d5cceaa
Federico Rossi <federico.rossi@phd.unipi.it>
2023-10-19 15:34:14 +0200

    Added source for the converter

---
+++ torch2pvs.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+import numpy
+import argparse
+
+# Constant blocks
+
+const_header = '''
+mlp: THEORY
+    BEGIN 
+        IMPORTING matrices@matrices
+'''
+
+const_trailer = "END mlp"
+
+const_mat_relu = '''
+    reluMat(M: Matrix): {A: MatrixMN(rows(M),columns(M))|
+        FORALL (i,j:below(length(M))): entry(A)(i,j) = relu(entry(M)(i,j))}  =
+        form_matrix(LAMBDA (i,j:nat): relu(entry(M)(i,j)), rows(M), columns(M));
+'''
+
+const_relu = "relu(x: real): real = IF x > 0 THEN x ELSE 0 ENDIF"
+
+const_network = "net(input: Matrix): Matrix ="
+
+
+def genMatrixDeclarations(model):
+    matrix_declarations = []
+    for i,layer in enumerate(model):
+        if isinstance(layer, nn.Linear):
+            weights = layer.weight
+            
+            # Weights must be transposed to match matrix matrix sizes
+            tr_weights = torch.transpose(weights, 0, 1)
+
+            w_cols = len(weights)
+            w_rows = len(weights[0])
+
+            pvs_matrix_entries = []
+            for row in tr_weights:
+                np_weight = row.detach().numpy()
+                np_str_weight = numpy.char.mod("%f", np_weight)
+                joined_str_weight = ','.join(np_str_weight)
+                pvs_formatted = "(:"+joined_str_weight+":)"
+                pvs_matrix_entries.append(pvs_formatted)
+
+            pvs_formatted_matrix = "(:"+','.join(pvs_matrix_entries)+":)"
+
+            pvs_fullmatrix_entry = "linear"+str(i)+": MatrixMN("+str(w_rows)+","+str(w_cols)+") = "+pvs_formatted_matrix
+            matrix_declarations.append(pvs_fullmatrix_entry)
+    return matrix_declarations
+
+def genNetworkOperationSequence(model):
+    network_operations = []
+    activation_idx_start = 0
+    num_layers = len(model)
+
+    for i, layer in enumerate(model):
+        if isinstance(layer, nn.Linear):
+            if i == 0:
+                network_operations.append("input")
+            network_operations.append("*linear"+str(i))
+        
+        if isinstance(layer, nn.ReLU):
+
+            # Insert relu at beginning of sequence and close bracket at end of sequence
+            network_operations.insert(0,"reluMat(")
+            network_operations.append(")")
+
+    return network_operations
+
+def genTheorem(name, input_vars, output_vars):
+    name+=": THEOREM\n"
+    name+="\t\tFORALL ("
+
+    xinvars = []
+    xnames = []
+    for i in range(input_vars):
+        xnames.append("x"+str(i)+"in")
+        xinvars.append("x"+str(i)+"in: x"+str(i)+"inreal")
+
+    name+=','.join(xinvars)+"):\n"
+    name+="\t\t\tentry( net( (:(:"+','.join(xnames)+":):) ) )(0,0) <TBD>"
+
+    return name
+
+def genConstraintExpressions(input_vars):
+    constraints = ""
+    for i in range(input_vars):
+        constraints+="\tx"+str(i)+"inreal: TYPE = { r: real | r>=-<TBD> AND r<=<TBD> }\n"
+    print(constraints)
+
+    return constraints
+
+# Read input arguments
+
+parser = argparse.ArgumentParser(
+                    prog='torch2pvs',
+                    description='Generate a skeleton PVS theory for a MLP torch model',
+                    epilog='Federico Rossi, 2023')
+
+parser.add_argument("model_path")
+parser.add_argument("-o","--output_vars", default=1, required=False, type=int)
+parser.add_argument("-p","--pvs_output", default=None, required=False)
+args = parser.parse_args()
+
+model = torch.load(args.model_path)
+
+input_vars = next(model.parameters()).size()[1]
+
+lines = genMatrixDeclarations(model)
+sequence = genNetworkOperationSequence(model)
+
+theorem = genTheorem("network_bounds",input_vars,args.output_vars)
+
+constraints = genConstraintExpressions(input_vars)
+
+# Emit PVS
+
+pvs_buffer = ""
+pvs_buffer+=const_header
+pvs_buffer+="\n\t"
+for line in lines:
+    pvs_buffer+=line+"\n\t"
+
+pvs_buffer+="\n\t"+const_relu
+pvs_buffer+="\n"+const_mat_relu
+
+pvs_buffer+="\n"+constraints
+
+pvs_buffer+="\n\t"+const_network
+pvs_buffer+="\t\t\n\t\t"+"".join(sequence)+"\n\n"
+
+pvs_buffer+="\t"+theorem
+pvs_buffer+="\n\n"+const_trailer
+
+if args.pvs_output is not None:
+    outfile = open(args.pvs_output, "w")
+    outfile.write(pvs_buffer)
+else:
+    print(pvs_buffer)

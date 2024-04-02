5f4d4462511bcce88327da981d797b9bbf0a58d0
Federico Rossi <f.rossi57@studenti.unipi.it>
2024-04-02 21:17:16 +0200

    Refactored

--- plot_reachability.py
+++ plot_reachability.py
@@ -6,9 +6,9 @@ import interval_tensor
 from interval_tensor import IntervalTensor
 
 
-def extract_output_tensor(interval_tensor):
-    idata = interval_tensor.data()
-    out_data = [0] * len(idata)
+def extract_feature_tensor_bounds(feature_tensor):
+    idata = feature_tensor.data()
+    out_data = [np.array([])] * len(idata)
     for l_, i_ in enumerate(idata):
         feat_ = np.array(i_[0]).reshape(2, 1)
         label_ = (np.ones(2) * l_).reshape(2, 1)
@@ -22,7 +22,9 @@ def extract_output_tensor(interval_tensor):
 # The internal layers do not matter for now, except for the activation functions
 net = nn.Sequential(
     nn.Linear(in_features=2, out_features=10, bias=True),
-    nn.ReLU(),
+    nn.Sigmoid(),
+    nn.Linear(in_features=10, out_features=10, bias=True),
+    nn.Sigmoid(),
     nn.Linear(in_features=10, out_features=4, bias=True),
     nn.Sigmoid()
 )
@@ -31,8 +33,6 @@ input_features = net[0].weight.shape[1]
 
 input_size = net[0].weight.shape[0]
 
-input_features
-
 # Select the sampling region for the inputs and sampling interval
 samples = 10000
 
@@ -40,8 +40,8 @@ samples = 10000
 torch.random.manual_seed(100)
 
 # Per feature uniform sampling
-feat_1 = torch.distributions.uniform.Uniform(-1, 1).sample([samples])
-feat_2 = torch.distributions.uniform.Uniform(-1, 1).sample([samples])
+feat_1 = torch.distributions.uniform.Uniform(9, 10).sample([samples])
+feat_2 = torch.distributions.uniform.Uniform(9, 10).sample([samples])
 
 # Combine the features
 feat = torch.stack([feat_1, feat_2], 1)
@@ -60,17 +60,17 @@ p1 = np.hstack((np.ones((len(poly_points_class_1), 1)), poly_points_class_1))
 p2 = np.hstack((2*np.ones((len(poly_points_class_2), 1)), poly_points_class_2))
 p3 = np.hstack((3*np.ones((len(poly_points_class_3), 1)), poly_points_class_3))
 
-intervals = np.array([[-1, 1], [-1, 1]], dtype=object)
+intervals = np.array([[9, 10], [9, 10]], dtype=object)
 i = IntervalTensor(intervals)
 o = net(i)
 
-bounds = extract_output_tensor(o)
+bounds = extract_feature_tensor_bounds(o)
 bb = np.array(bounds).reshape(8, 2)
 
-plt.scatter(p0[:, 1], p0[:, 0], s=10, marker="+")
-plt.scatter(p1[:, 1], p1[:, 0], s=10, marker="+")
-plt.scatter(p2[:, 1], p2[:, 0], s=10, marker="+")
-plt.scatter(p3[:, 1], p3[:, 0], s=10, marker="+")
+plt.scatter(p0[:, 1], p0[:, 0], s=0.60, marker="+")
+plt.scatter(p1[:, 1], p1[:, 0], s=0.60, marker="+")
+plt.scatter(p2[:, 1], p2[:, 0], s=0.60, marker="+")
+plt.scatter(p3[:, 1], p3[:, 0], s=0.60, marker="+")
 plt.scatter(bb[:, 1], bb[:, 0], color="green", marker="*")
 plt.ylabel("Label")
 plt.xlabel("Score")
